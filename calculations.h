efe9e06848d43d3ab1f78c968782b90f90701938
tomasjon1 <72403454+tomasjon1@users.noreply.github.com>
2022-04-21 23:42:34 +0300

    Delete calculations.h

--- calculations.h
+++
@@ -1,6 +0,0 @@
-
-#include "data.h"
-#include "validation.h"
-
-void galutiniai(deque<studentas> &studentai);
-void sortStudents(deque<studentas> &kietiakai, deque<studentas> &vargsai, deque<studentas> &studentai);
